331463c3b718e97226b969bdd0366bc31bb226fd
SRxMNz <edna32.limalima@gmail.com>
2025-10-02 20:21:38 -0300

    atualizações

--- app/src/main/java/br/com/etecia/enigmaapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/enigmaapp/MenuActivity.java
@@ -1,8 +1,6 @@
 package br.com.etecia.enigmaapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.splash_layout);
+        setContentView(R.layout.menu_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
-
-            }
-        },2000);
     }
 }
